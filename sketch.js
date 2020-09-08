c060608b61baebca62cb21307f030637f42fdefa
dhanyadeep <70516520+dhanyadeep@users.noreply.github.com>
2020-09-08 13:54:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+var ground;
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box1.shapeColor="yellow";
+
+    box2=new Box(240,100,50,100);
+box2.shapeColor="yellow";
+
+    ground=new Ground(200,370,400,40);
+    ground.shapeColor="white";
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+   
+}
